94b525251bf18e9ba89fb8f757d8e1c146de3eaf
takuma416 <k024g1026@g.neec.ac.jp>
2025-05-08 16:14:37 +0900

    ブロック

--- Player.h
+++ Player.h
@@ -6,4 +6,10 @@ public:
 	void Initialize(KamataEngine::Model* model, uint32_t textureHandle, KamataEngine::Camera* camera);
 	void Update();
 	void Draw();
+
+private:
+	KamataEngine::WorldTransform worldTransform_;
+	KamataEngine::Model* model_ = nullptr;
+	uint32_t textureHandle_ = 0u;
+	KamataEngine::Camera* camera_ = nullptr;
 };
